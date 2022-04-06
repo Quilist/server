cf73357a77ddbf040629c0b8213148c225a880c8
taopsll <taopsll22@gmail.com>
2022-04-06 12:59:49 +0300

    fix

--- db/dbRequests.js
+++ db/dbRequests.js
@@ -1,5 +1,5 @@
 // Бд запросы на удаление, получения списка, получение по айди
-const getItem = (...item) => `SELECT ${item[1] || "*"} FROM ${item} WHERE id = ?`;
+const getItem = (...item) => `SELECT ${item[1] || "*"} FROM ${item[0]} WHERE id = ?`;
 const getItems = (...item) => `SELECT ${item[1] || "*"} FROM ${item[0]} WHERE id_user = ?`;
 const removeItem = item => `DELETE FROM ${item} WHERE id = ?`;
 
