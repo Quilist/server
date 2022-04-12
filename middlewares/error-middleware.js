cf830301f0890d6ab04b83b6726a2b2a23f59a80
taopsll <taopsll22@gmail.com>
2022-04-12 21:05:19 +0300

    commit

--- middlewares/error-middleware.js
+++ middlewares/error-middleware.js
@@ -3,7 +3,7 @@ const apiError = require("../exceptions/error");
 module.exports = (err, req, res, next) => {
 
     if (err instanceof apiError) {
-        return res.status(err.status).json({ status: "error", message: err.error })
+        return res.status(err.status).json({ status: "error", message: err.message })
     }
 
     return res.status(500).json({ status: "error", message: err.message });
