ce06e5a93274596da48776c871fdf0c7373b824b
taopsll <taopsll22@gmail.com>
2022-04-12 14:11:43 +0300

    commit

--- middlewares/error-middleware.js
+++ middlewares/error-middleware.js
@@ -6,5 +6,5 @@ module.exports = (err, req, res, next) => {
         return res.status(err.status).json({ status: "error", message: err.error })
     }
     
-    return res.status(500).json({ status: "error", message: "Something broke!" });
+    return res.status(500).json({ status: "error", message: err.message });
 }
