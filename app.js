9be72d68b1199bf99c45703d49902678662b7794
taopsll <taopsll22@gmail.com>
2022-04-12 23:08:32 +0300

    commit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 const apiRouter = require('./routes/routes');
 
 const allowCrossDomain = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Origin', "https://b-fin.tech");
   res.header('Access-Control-Allow-Headers', "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   res.header('Access-Control-Allow-Methods', "GET, OPTIONS, POST, PUT");
   res.header('Access-Control-Allow-Credentials', "true")
