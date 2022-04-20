5ce69754c3cb4e06cc9e50dbfd25ed1f1b9998ca
Quilist <93136218+Quilist@users.noreply.github.com>
2022-04-20 21:10:06 +0300

    Add files via upload

--- routes/product/product.js
+++ routes/product/product.js
@@ -2,27 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const prisma = require("../../database/database");
+const itemsController = require("../../controllers/items/items-controller");
 
-router.get("/", (req, res) => {
-  const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 25;
-
-  prisma.products.findMany({ skip: limit * (page - 1), take: limit, where: { id_user: Number(req.token.id) } })
-    .then(async (result) => {
-      const total = await prisma.products.count();
-
-      res.json({
-        status: "OK", message: {
-          items: result,
-          paginations: {
-            total: total,
-            last_page: total <= limit ? 1 : total / limit
-          }
-        }
-      });
-    })
-    .catch(err => res.json({ status: "error", message: err.message }));
-});
+router.get("/", itemsController.all);
+router.get("/:id", itemsController.id);
+router.post("/:id/edit", itemsController.edit);
+router.post("/:id/remove", itemsController.delete);
 
 router.post("/add", async (req, res) => {
   const dateMs = String(Date.now());
@@ -38,67 +23,26 @@ router.post("/add", async (req, res) => {
   }
 
   try {
-    const product = await prisma.products.create({ data: data })
-    if (amountData && amountData.length > 0) {
-      let subData = [];
-      amountData.forEach(function (item) {
-        subData.push({
-          ...item,
+    const product = await prisma.products.create({ data: data });
+
+    if (amountData.length) {
+
+      const subData = amountData.map(elem => {
+        return {
+          ...elem,
           product_id: product.id,
           created_at: dateMs,
           updated_at: dateMs
-        })
+        }
       });
 
-      await prisma.products_amount_data.createMany({
-        data: subData
-      })
+      await prisma.products_amount_data.createMany({ data: subData });
     }
 
-    res.json({
-      status: "OK",
-      message: "Success"
-    })
+    res.json({ status: "OK", message: "Success" });
   } catch (e) {
-    console.log(e)
-    throw e
+    res.json({ status: "error", message: e.message });
   }
-});
-
-
-router.get("/:id", (req, res) => {
-
-  prisma.products.findUnique({ where: { id: Number(req.params.id) } })
-    .then((result) => {
-      if (!result) return res.json({ status: "error", message: "Unknown id" });
-
-      if (result.id_user !== req.token.id) {
-        return res.json({ status: "error", message: "Action not allowed" });
-      }
-
-      res.json({ status: "OK", message: result });
-    })
-    .catch(err => res.json({ status: "error", message: err.message }));
-});
-
-
-router.post("/:id/edit", (req, res) => {
-  const dateMs = String(Date.now());
-
-  const data = {
-    ...req.body,
-    updated_at: dateMs
-  }
-
-  prisma.products.update({ data: { ...data }, where: { id: Number(req.params.id) } })
-    .then(() => res.json({ status: "OK", message: "Succes" }))
-    .catch(err => res.json({ status: "error", message: err.message }));
-});
-
-router.post("/:id/remove", (req, res) => {
-  prisma.products.delete({ where: { id: Number(req.params.id) } })
-    .then(() => res.json({ status: "OK", message: "Succes" }))
-    .catch(err => res.json({ status: "error", message: err.message }));
 });
 
 router.get("/auxiliary/data", async (req, res) => {
@@ -113,7 +57,7 @@ router.get("/auxiliary/data", async (req, res) => {
 
   Promise.all(promises)
     .then(elem => {
- 
+
       const data = {
         storehouses: elem[0],
         type_prices: elem[1],
@@ -124,12 +68,9 @@ router.get("/auxiliary/data", async (req, res) => {
         currencies: elem[5]
       };
 
-      res.json({
-        status: "OK", message: data
-      });
+      res.json({ status: "OK", message: data });
     })
-    .catch(({ message }) => res.json({ status: "error", message }));
-
+    .catch(e => res.json({ status: "error", message: e.message }));
 });
 
 module.exports = router;
