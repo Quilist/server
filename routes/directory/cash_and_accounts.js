1d8d67196c6b1f99bbb4f0772ca0343591c9a2be
Quilist <93136218+Quilist@users.noreply.github.com>
2022-04-26 17:31:39 +0300

    Add files via upload

--- routes/directory/cash_and_accounts.js
+++ routes/directory/cash_and_accounts.js
@@ -81,7 +81,7 @@ router.get("/auxiliary/data", async (req, res) => {
 });
 
 // router.get("/:id")
-router.post("/:id/remove", (req, res) => {
+router.post("/:id/remove", async (req, res) => {
   const cashAccounts = await prisma.cash_accounts.delete({ where: { id: Number(req.params.id) } });
   const cashBalance = await prisma.cash_accounts.delete({ where: { cash_account_id: Number(req.params.id) } });
 
