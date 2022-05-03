58ecd0b0ed76f3bf16ed1b7ec866cdd88120f1a9
Quilist <93136218+Quilist@users.noreply.github.com>
2022-05-03 17:16:55 +0300

    Update privat24.js

--- services/banks/privat24.js
+++ services/banks/privat24.js
@@ -18,15 +18,15 @@ async function individualInfo(card) {
 
 async function entityInfo(id, token) {
     const response = await fetch("https://acp.privatbank.ua/api/statements/balance/final?limit=1000", {
-        headers: {
+        'headers': {
             'id': id,
             'token': token,
-            'Content-type': 'application/json;charset=utf8'
+            'Content-type': 'application/json;charset=cp1251'
         }
     });
 
     const result = await response.json();
-    console.log(result)
+    console.log(res)
     const array = result.balances.filter(elem => elem.balanceIn !== '0.00')
 
     return array;
