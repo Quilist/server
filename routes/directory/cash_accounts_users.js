8ff5207c6f88b4c0da5f74b95ccf99bb75ec8fe2
Quilist <93136218+Quilist@users.noreply.github.com>
2022-05-06 19:16:47 +0300

    Update cash_accounts_users.js

--- routes/directory/cash_accounts_users.js
+++ routes/directory/cash_accounts_users.js
@@ -3,10 +3,39 @@ const router = express.Router();
 
 const itemsController = require("../../controllers/items/items-controller");
 
+router.get("/", async (req, res, next) => {
+  try {
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 25;
+
+    const data = await prisma.cash_accounts_users.findMany({
+      skip: limit * (page - 1),
+      take: limit,
+      where: { id_user: token.id },
+      include: {
+        cash_account: true
+      }
+    });
+
+    const total = await prisma.cash_accounts_users.count({ where: { id_user: token.id } });
+
+    return res.json({
+      status: "OK", message: {
+        items: data,
+        paginations: {
+          total: total,
+          last_page: total <= limit ? 1 : total / limit
+        }
+      }
+    })
+  } catch (e) {
+    next(e)
+  }
+});
+
 router.post("/:id/edit", itemsController.edit);
 router.post("/add", itemsController.add);
 router.post("/:id/remove", itemsController.delete);
-router.get("/", itemsController.all);
 router.get("/:id", itemsController.id);
 
 module.exports = router;
