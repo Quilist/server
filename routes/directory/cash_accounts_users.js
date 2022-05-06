288cc3524470f8c9b96471414f40abce246127ee
Quilist <93136218+Quilist@users.noreply.github.com>
2022-05-06 19:17:30 +0300

    Update cash_accounts_users.js

--- routes/directory/cash_accounts_users.js
+++ routes/directory/cash_accounts_users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const itemsController = require("../../controllers/items/items-controller");
+const prisma = require("../../database/database");
 
 router.get("/", async (req, res, next) => {
   try {
