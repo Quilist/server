be4ce74d9bcb52c0580b247a4bf1fa35d1452b33
Quilist <93136218+Quilist@users.noreply.github.com>
2022-05-18 19:36:08 +0300

    Update cash_and_accounts.js

--- routes/directory/cash_and_accounts.js
+++ routes/directory/cash_and_accounts.js
@@ -178,6 +178,7 @@ router.post("/add", async (req, res) => {
 
     res.json({ status: "OK", message: "Success" });
   } catch (e) {
+    console.log(e)
     res.json({ status: "error", message: e.message });
   }
 });
