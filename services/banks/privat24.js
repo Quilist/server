93d0fc918c6eb02699c795538e43ec0d0ccee03e
Quilist <93136218+Quilist@users.noreply.github.com>
2022-06-10 20:16:59 +0300

    Add files via upload

--- services/banks/privat24.js
+++ services/banks/privat24.js
@@ -37,7 +37,7 @@ async function entityInfo(id, token) {
 }
 
 
-async function entityTransation(id, token, date) {
+async function entityTransation(id, token, acc, date) {
     const headers = {
         'headers': {
             'id': id,
@@ -46,10 +46,10 @@ async function entityTransation(id, token, date) {
         }
     }
 
-    const transationsResponse = await fetch(`https://acp.privatbank.ua/api/statements/transactions?startDate=${date}&limit=500`, headers);
+    const transationsResponse = await fetch(`https://acp.privatbank.ua/api/statements/transactions?acc=${acc}&startDate=${date}&limit=500`, headers);
     const resultTransations = await transationsResponse.json();
 
-    return resultTransations?.transactions
+    return resultTransations;
 }
 
 module.exports = {
@@ -58,3 +58,5 @@ module.exports = {
     entityInfo,
     entityTransation
 }
+
+{/* <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="48" height="48" viewBox="0 0 172 172" style=" fill:#26e07f;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#1fb141"><path d="M21.5,21.5v129h64.5v-32.25v-64.5v-32.25zM86,53.75c0,17.7805 14.4695,32.25 32.25,32.25c17.7805,0 32.25,-14.4695 32.25,-32.25c0,-17.7805 -14.4695,-32.25 -32.25,-32.25c-17.7805,0 -32.25,14.4695 -32.25,32.25zM118.25,86c-17.7805,0 -32.25,14.4695 -32.25,32.25c0,17.7805 14.4695,32.25 32.25,32.25c17.7805,0 32.25,-14.4695 32.25,-32.25c0,-17.7805 -14.4695,-32.25 -32.25,-32.25z"></path></g></g></svg> */}
